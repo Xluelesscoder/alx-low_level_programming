856619cccf2cc1306e3d471f6f752f5e92775ca9
Xluelesscoder <issakaabdulrashid1@gmail.com>
2023-02-16 15:32:26 -0800

    print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program thats prints and line with puts
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
