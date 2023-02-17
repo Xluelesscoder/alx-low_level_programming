f9c4635ece92bd17572c87dabfda0b1a166b97b5
Xluelesscoder <issakaabdulrashid1@gmail.com>
2023-02-16 16:37:42 -0800

    size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
- *main -Entry point Program that prints size of various types on data types
+ *main -Entry point Program prints size of various types on data types
  *Return: 0
 */
-
 int main(void)
 {
-
-printf("Size of a char : %lubyte(s)\n", (unsigned long) sizeof(char));
-printf("Size of an int : %lubyte(s)\n", (unsigned long) sizeof(int));
-printf("Size of a long int : %lubyte(s)\n", (unsigned long) sizeof(long int));
-printf("Size of a long long int : %lubyte(s)\n", (unsigned long) sizeof(long long int));
-printf("Size of a float : %lubyte(s)\n", (unsigned long) sizeof(float));
+long long int d;
+printf("Size of a char : %lu byte(s)\n", (unsigned long) sizeof(char));
+printf("Size of an int : %lu byte(s)\n", (unsigned long) sizeof(int));
+printf("Size of a long int : %lu byte(s)\n", (unsigned long) sizeof(long int));
+printf("Size of a long long int : %lu byte(s)\n", (unsigned long) sizeof(d));
+printf("Size of a float : %lu byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
