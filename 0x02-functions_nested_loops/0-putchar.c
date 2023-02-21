30a00932768229dfd74b1a45cf243412933ff295
Xluelesscoder <issakaabdulrashid1@gmail.com>
2023-02-21 15:59:58 -0800

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 	/**
-	 * main - print -putchar
+	 *main -Entry point print -putchar
 	 * 
 	 *
 	 * Return: 0 On success
@@ -8,14 +8,14 @@
 	 */
  int  main(void)
 {
-	_putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
  return (0);
 }
