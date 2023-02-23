f172dee79f28fb77c3886074ce1d8e2120fe6879
Xluelesscoder <issakaabdulrashid1@gmail.com>
2023-02-23 09:33:01 -0800

    hmm

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+ int largest;
 
 if (a > b && a > c)
 {
@@ -22,7 +22,7 @@ largest = c;
 }
 else if (b > c)
 {
-largest = b
+largest = b;
 }
 else
 {
