43024c14ad3306ffdc2105b417279ad7883103b7
Xluelesscoder <issakaabdulrashid1@gmail.com>
2023-04-04 10:44:22 -0700

    number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len -counts the number of elements in linked listint_t
+ * @h: List to check
+ * Return: number of elements in list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number_of_elements = 0;
+	const listint_t *p = h;
+
+	while (p != NULL)
+	{
+		number_of_elements++;
+		p = p->next;
+	}
+	return (number_of_elements);
+}
